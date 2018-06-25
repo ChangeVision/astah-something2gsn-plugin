c71561055d333e26959bd5aeb3b223a2eafd8d2b
toshinoritakai <toshinori.takai@change-vision.com>
2018-06-26 07:09:47 +0900

    ・astahを日本語版で立ち上げたときに、英語でGSNのノードの型名を指定したときの問題を修正 ・Kotlinのバージョンをあげた ・プラグインのバージョンをあげた

--- src/main/java/astah/plugin/SacmForGsn/GSNType.kt
+++ src/main/java/astah/plugin/SacmForGsn/GSNType.kt
@@ -5,40 +5,44 @@ import java.util.*
 enum class GSNType {
     Goal {
         override fun toString(): String = rb.getString("goal_id")
+        override fun toJpString(): String = rb.getString("goal_jp_id")
     },
     Strategy {
         override fun toString(): String = rb.getString("strategy_id")
+        override fun toJpString(): String = rb.getString("strategy_jp_id")
     },
     Solution {
         override fun toString(): String = rb.getString("solution_id")
+        override fun toJpString(): String = rb.getString("solution_jp_id")
     },
     Context {
         override fun toString(): String = rb.getString("context_id")
+        override fun toJpString(): String = rb.getString("context_jp_id")
     },
     Justification {
         override fun toString(): String = rb.getString("justification_id")
+        override fun toJpString(): String = rb.getString("justification_jp_id")
     },
     Assumption {
         override fun toString(): String = rb.getString("assumption_id")
+        override fun toJpString(): String = rb.getString("assumption_jp_id")
     };
     abstract override fun toString() : String
+    abstract fun toJpString() : String
     companion object {
+        val rb = ResourceBundle.getBundle("plugin")
         fun obtainType(s : String?) : GSNType? {
             return s?.let {
                 when (s.toLowerCase()) {
-                    Goal.toString() -> Goal
-                    Strategy.toString() -> Strategy
-                    Solution.toString() -> Solution
-                    Context.toString() -> Context
-                    Justification.toString() -> Justification
-                    Assumption.toString() -> Assumption
+                    Goal.toString(), Goal.toJpString() -> Goal
+                    Strategy.toString(), Strategy.toJpString() -> Strategy
+                    Solution.toString(), Solution.toJpString() -> Solution
+                    Context.toString(), Context.toJpString() -> Context
+                    Justification.toString(), Justification.toJpString() -> Justification
+                    Assumption.toString(), Assumption.toJpString() -> Assumption
                     else -> null
                 }
             }
         }
     }
-    val rb = ResourceBundle.getBundle("plugin")
-    val rb_ja = ResourceBundle.getBundle("plugin_ja")
-    private fun checkId(toBeChecked : String, key : String): Boolean =
-            toBeChecked.equals(rb.getString(key)) || toBeChecked.equals(rb_ja.getString(key))
 }
